9e6575ea748d44a0d0621b62efbb689d04e13f08
Yevheniia Mishchishyna <janekaraim@gmail.com>
2024-05-25 11:38:50 +0300

    Ordered linked list added

--- linked_list.py
+++ linked_list.py
@@ -1,12 +1,43 @@
 class Node:
 
-    def __init__(self, _id, value):
+    def __init__(self, _id, value, next_node=None):
         self.id = _id
         self.value = value
-        self.next = None
+        self.next = next_node
 
     def __str__(self):
         return f"Node {self.id}: value {self.value}"
+
+class OrderedLinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def __len__(self):
+        counter = 0
+        current = self.head
+        while current:
+            counter += 1
+            current = current.next
+        return counter
+
+    def add_node(self, value):
+        _id = len(self) + 1
+        if not self.head or self.head.value > value:
+            self.head = Node(_id, value, self.head)
+        else:
+            current = self.head
+            while current.next:
+                if current.next.value > value:
+                    break
+                current = current.next
+            current.next = Node(_id, value, current.next)
+
+    def print_nodes(self):
+        current = self.head
+        while current:
+            print(current)
+            current = current.next
 
 class LinkedList:
 
@@ -45,12 +76,24 @@ class LinkedList:
             current = current.next
 
 if __name__ == "__main__":
+    """
     container = LinkedList()
     container.add_node(10)
     container.add_node(20)
     container.add_node(30)
     print(len(container))
     container.print_nodes()
+    """
 
+    ordered_container = OrderedLinkedList()
+    ordered_container.add_node(40)
+    ordered_container.add_node(20)
+    ordered_container.add_node(60)
+    ordered_container.add_node(50)
+    ordered_container.add_node(30)
+    ordered_container.print_nodes() #20, 30, 40, 50, 60
+
+    """
     for node in container:
         print(node)
+    """
