522974842c36645c195ab83567580bb58fb6cb49
Yevheniia Mishchishyna <janekaraim@gmail.com>
2024-06-29 13:43:04 +0300

    Django models

--- social_network/content/models.py
+++ social_network/content/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    # id = PrimaryKey, implicit
+    title = models.CharField(max_length=100, default="No Title")
+    text = models.TextField()
+
+    def __str__(self):
+        return f"Post #{self.id}: {self.title}"
+
+class Comment(models.Model):
+
+    # id
+    post = models.ForeignKey(
+        "Post", on_delete=models.CASCADE, related_name="comments"
+    )
+    text = models.TextField()
+    modified = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment #{self.id} for {self.post}"
